8b0b7a51d91c73c5899f936b44d0c3ae365af7f7
msosborne3 <msosborne@ymail.com>
2017-02-16 15:28:36 -0600

    added a workouts page that lists all of the current user's workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,8 +1,22 @@
 class WorkoutsController < ApplicationController
+  before_action :set_workout, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @workouts = current_user.workouts
+  end
+
   def new
+    @workout = Workout.new
   end
 
   def create
+    @workout = Workout.new(user_id: current_user.id)
+    @workout.update(workout_params)
+    if @workout.save
+      redirect_to workout_path(@workout)
+    else
+      redirect_to controller: "workouts", action: "new"
+    end
   end
 
   def show
@@ -12,8 +26,29 @@ class WorkoutsController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @workout.update(workout_params)
+        format.html { redirect_to @workout, notice: 'Workout was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def destroy
+    @workout.destroy
+    respond_to do |format|
+      format.html { redirect_to user_path(current_user), notice: 'Workout was successfully deleted.' }
+    end
+  end
+
+  private
+
+  def set_workout
+    @workout = Workout.find_by(id: params[:id])
+  end
+
+  def workout_params
+    params.require(:workout).permit(:content, :calories_burned)
   end
 end
