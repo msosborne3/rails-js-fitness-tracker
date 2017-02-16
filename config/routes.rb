0a9d4bb034d9f91b5b13d4909c06e601d123c415
msosborne3 <msosborne@ymail.com>
2017-02-16 13:18:31 -0600

    generated workouts controller through rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'workouts/new'
+
+  get 'workouts/create'
+
+  get 'workouts/show'
+
+  get 'workouts/edit'
+
+  get 'workouts/update'
+
+  get 'workouts/destroy'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
