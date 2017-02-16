08328007dd8a14c44f3b19f14910778f59d99813
msosborne3 <msosborne@ymail.com>
2017-02-16 13:09:55 -0600

    added has_many and belongs_to associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  has_many :supports
+  belongs_to :user
+  has_many :supporters, :through => :supports, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :workouts
+  has_many :meals
+  has_many :posts
+  has_many :supports
 end
