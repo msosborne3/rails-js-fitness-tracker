95a6b16707ebe77bd717aa2b6e21b1adcbe77c78
msosborne3 <msosborne@ymail.com>
2017-02-16 13:19:57 -0600

    added resources for workouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'workouts/new'
-
-  get 'workouts/create'
-
-  get 'workouts/show'
-
-  get 'workouts/edit'
-
-  get 'workouts/update'
-
-  get 'workouts/destroy'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # homepage
   root 'static#home'
+
+  # workouts
+  resources :workouts
 end
